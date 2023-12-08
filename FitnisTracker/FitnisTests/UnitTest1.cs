5e848ef7bcbc6477130756e3ec8fed19d0c093f6
Dylan Sacco <Dms64@students.ptcollege.edu>
2023-12-08 18:40:49 -0500

    Created Project - Extra
    In this verson i created the project and the test project. Cottrell did say that we could all use the same tests, but we had to have some unique test. My Idea is that we can all make test for the app, but for what you will be desiging specifically, id imageine we will have a good some of 10-15 ish test that we all submit, and like 3- 5  unique tests in our submition. Be sure to label your Tests with like comments so when it comes time to get unique test you can easyly find yours.

---
+++ FitnisTracker/FitnisTests/UnitTest1.cs
@@ -0,0 +1,34 @@
+﻿namespace FitnisTests;
+using FitnisTracker.Models;
+
+public class UnitTest1
+{
+    public UserModel Person;
+
+    [Fact]
+    public void Test1()
+    {
+
+    }
+
+    // Test UserModel
+    [Fact]
+    public void TextConstructor()
+    {
+        
+    }
+
+    [Fact]
+    public void TestDataSet1()
+    {
+        int age = 19;
+        int feet = 5;
+        int inch = 11;
+        inch += feet * 12;
+        double WeightLb = 130;
+        Activity activity = Activity.Sedentary;
+        int CaloriesMaintain = 1952;
+        int CaloriesWeightLoss = 1452;
+
+    }
+}
---
+++ FitnisTracker/FitnisTracker/Models/UserModel.cs
@@ -0,0 +1,36 @@
+﻿using System;
+namespace FitnisTracker.Models
+{
+    public enum Activity
+    {
+        BasalMetabolicRate, //(BMR)
+        Sedentary, // little or no exercise
+        Light, // exercise 1-3 times/week
+        Moderate, // exercise 4-5 times/week
+        Active, // daily exercise or intense exercise 3-4 times/week
+        VeryActive,// intense exercise 6-7 times/week
+        ExtraActive// very intense exercise daily, or physical job
+    }
+
+    public class UserModel
+	{
+
+		public double CurrentWeight { get; private set; }
+		public double DesiredWeight { get; private set; }
+		public double Height_In { get; private set; }
+		public String Gender { get; private set; }
+		public Activity ActivityLevel { get; private set; } 
+		public DateTime Birthday { get; private set; }
+		public int Age { get; } // this will be calculated not requested
+		public int CalorieLimit { get; private set; }
+		public String Name { get; private set; }
+		public String Email { get; private set; }
+		// feel free to add more
+
+        public UserModel()
+		{
+
+		}
+	}
+}
+
