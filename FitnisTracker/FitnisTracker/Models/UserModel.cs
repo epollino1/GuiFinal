c38eef1c978911dfeb4fe8daf01d7098a82b929e
AmirFadel <115519463+AmirFadel@users.noreply.github.com>
2023-12-14 16:40:14 -0500

    re-edited the usermodel.cs class
    changed the setters to public because private setters break asp-for

--- FitnisTracker/FitnisTracker/Models/UserModel.cs
+++ FitnisTracker/FitnisTracker/Models/UserModel.cs
@@ -1,37 +1,52 @@
 ﻿using System;
 namespace FitnisTracker.Models
 {
-    public enum FitnisActivity
-    {
-        BasalMetabolicRate, //(BMR)
-        Sedentary, // little or no exercise
-        Light, // exercise 1-3 times/week
-        Moderate, // exercise 4-5 times/week
-        Active, // daily exercise or intense exercise 3-4 times/week
-        VeryActive,// intense exercise 6-7 times/week
-        ExtraActive// very intense exercise daily, or physical job
-    }
-
     public class UserModel
-	{
+    {
+        [Required]
+        public double CurrentWeight { get; set; }
 
-		public double CurrentWeight { get; private set; }
-		public double DesiredWeight { get; private set; }
-		public double Height_In { get; private set; }
-		public String Gender { get; private set; }
-		public FitnisActivity ActivityLevel { get; private set; } 
-		public DateTime Birthday { get; private set; }
-		public int Age { get; } // this will be calculated not requested
-		public int CalorieLimit { get; private set; }
-		public String Name { get; private set; }
-		public String Email { get; private set; }
-		// feel free to add more
+        [Required]
+        public double DesiredWeight { get; set; }
+
+        [Required]
+        public double Height_In { get; set; }
+
+        [Required]
+        public string Gender { get; set; }
+
+        [Required]
+        public Activity ActivityLevel { get; set; }
+
+        [Required]
+        public DateTime Birthday { get; set; }
+
+        public int Age
+        {
+            get
+            {
+                DateTime today = DateTime.Today;
+                int age = today.Year - Birthday.Year;
+                if (Birthday.Date > today.AddYears(-age))
+                {
+                    age--;
+                }
+                return age;
+            }
+        }
+
+        [Required]
+        public int CalorieLimit { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public string Email { get; set; }
 
         public UserModel()
 		{
-			Gender = "";
-			Name = "";
-			Email = "";
+			
 		}
 	}
 }
