e4ddbd6a3019ca5dda9c4539e04e986c556f3253
AmirFadel <115519463+AmirFadel@users.noreply.github.com>
2023-12-20 05:34:35 -0500

    finished regi and recalibrate stuff

--- FitnisTracker/FitnisTracker/Models/User.cs
+++ FitnisTracker/FitnisTracker/Models/User.cs
@@ -7,62 +7,59 @@ namespace FitnisTracker.Models;
 
 public partial class User
 {
-    
+
     public string UserId { get; set; } = null!;
-    
+
     public string? Username { get; set; }
-   
+
     public string? Email { get; set; }
-   
+
     public double? StartingWeight { get; set; }
-   
+
     public double? CurrentWeight { get; set; }
-   
+
     public double? DesiredWeight { get; set; }
-    
+
     public double? HeightIn { get; set; }
-  
+
     public string? Gender { get; set; }
-   
+
     public byte[]? Birthday { get; set; }
-   
+
     public long? Age { get; set; }
-   
+
     public long? CalorieLimit { get; set; }
-    
+
     public string? Activity { get; set; }
 
     public virtual ICollection<CaloryLog> CaloryLogs { get; set; } = new List<CaloryLog>();
 
     public virtual ICollection<WeightLog> WeightLogs { get; set; } = new List<WeightLog>();
 
-    public double CalculateBMR(string Gender, double currentWeight, double age, double height)
+    public double CalculateBMR(string gender, double currentWeight, double age, double heightInInches)
     {
-      
-        if (Gender == "male")
+        double heightInCentimeters = heightInInches * 2.54;
+
+        if (gender == "male")
         {
             return 88.362 + (13.397 * currentWeight) +
-                   (4.799 * height) - (5.677 * age);
+                   (4.799 * heightInCentimeters) - (5.677 * age);
         }
-        else if (Gender == "female")
+        else if (gender == "female")
         {
             return 447.593 + (9.247 * currentWeight) +
-                   (3.098 * height) - (4.330 * age);
+                   (3.098 * heightInCentimeters) - (4.330 * age);
         }
         return 0;
-
     }
     public void CalculateCalorieIntakeForWeightLoss()
     {
         double bmr = CalculateBMR(this.Gender, (double)this.CurrentWeight, (double)this.Age, (double)this.HeightIn);
         double activityFactor = 1.0;
-        if (Activity == null)
+
+        if (Activity != null)
         {
-            activityFactor = 1.0;
-        }
-        else
-        {
-            switch (this.Activity.ToLower()) // Convert input to lowercase for easier comparison
+            switch (this.Activity.ToLower())
             {
                 case "sedentary":
                     activityFactor = 1.2;
@@ -86,13 +83,14 @@ public partial class User
                     activityFactor = 1.0; // base BMR
                     break;
             }
-
         }
 
+        double maintenanceCalories = bmr * activityFactor;
+
+
         double calorieDeficitPerDay = 2 * 7700 / 7; // 2 lbs = 7700 calories
-        double calorieIntakeForWeightLoss = (bmr * activityFactor) - calorieDeficitPerDay;
+        double calorieIntakeForWeightLoss = maintenanceCalories - calorieDeficitPerDay;
 
         CalorieLimit = (long)calorieIntakeForWeightLoss;
     }
 }
-
